e31dc17c10af25628f4244e80a21139f6982acb7
csviri <csviri@gmail.com>
2023-02-28 15:46:08 +0100

    fix: folder key

--- operator/src/main/java/io/javaoperatorsdk/operator/longevitytest/ConfigMapReconciler.java
+++ operator/src/main/java/io/javaoperatorsdk/operator/longevitytest/ConfigMapReconciler.java
@@ -1,40 +1,43 @@
 package io.javaoperatorsdk.operator.longevitytest;
 
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import io.fabric8.kubernetes.api.model.ConfigMap;
 import io.fabric8.kubernetes.api.model.Secret;
 import io.javaoperatorsdk.operator.api.config.informer.InformerConfiguration;
 import io.javaoperatorsdk.operator.api.reconciler.*;
 import io.javaoperatorsdk.operator.processing.event.source.EventSource;
 import io.javaoperatorsdk.operator.processing.event.source.informer.InformerEventSource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.util.Map;
 
 @ControllerConfiguration(labelSelector = "type=longevity-test")
-public class ConfigMapReconciler implements Reconciler<ConfigMap>, EventSourceInitializer<ConfigMap> {
+public class ConfigMapReconciler
+    implements Reconciler<ConfigMap>, EventSourceInitializer<ConfigMap> {
 
-    private static final Logger log = LoggerFactory.getLogger(ConfigMapReconciler.class);
+  private static final Logger log = LoggerFactory.getLogger(ConfigMapReconciler.class);
 
-    @Override
-    public UpdateControl<ConfigMap> reconcile(ConfigMap configMap, Context<ConfigMap> context) {
-        log.info("Reconciling ConfigMap name: {}, namespace: {}",
-                configMap.getMetadata().getName(),
-                configMap.getMetadata().getNamespace());
+  @Override
+  public UpdateControl<ConfigMap> reconcile(ConfigMap configMap, Context<ConfigMap> context) {
+    log.info("Reconciling ConfigMap name: {}, namespace: {}",
+        configMap.getMetadata().getName(),
+        configMap.getMetadata().getNamespace());
 
-        var maybeSecret = context.getSecondaryResource(Secret.class);
-//        maybeSecret.ifPresentOrElse(,()->{
-//
-//        });
+    var maybeSecret = context.getSecondaryResource(Secret.class);
+    // maybeSecret.ifPresentOrElse(,()->{
+    //
+    // });
 
-        return UpdateControl.noUpdate();
-    }
+    return UpdateControl.noUpdate();
+  }
 
 
-    @Override
-    public Map<String, EventSource> prepareEventSources(EventSourceContext<ConfigMap> context) {
-        InformerEventSource<Secret, ConfigMap> secretES = new InformerEventSource<>(InformerConfiguration.from(Secret.class,context)
-                .build(), context);
-        return EventSourceInitializer.nameEventSources(secretES);
-    }
+  @Override
+  public Map<String, EventSource> prepareEventSources(EventSourceContext<ConfigMap> context) {
+    InformerEventSource<Secret, ConfigMap> secretES =
+        new InformerEventSource<>(InformerConfiguration.from(Secret.class, context)
+            .build(), context);
+    return EventSourceInitializer.nameEventSources(secretES);
+  }
 }
